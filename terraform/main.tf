e322d62453ddd01e23fdbc3321fa82fce175ff11
Cameron Stokes <cameron@cameronstokes.com>
2015-11-09 08:41:38 -0800

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+output "public_ip" {
+  value = "${aws_instance.httpd.public_ip}"
+}
+
+resource "atlas_artifact" "httpd" {
+  name = "clstokes/atlas-pipeline-httpd"
+  type = "amazon.image"
+}
+
+resource "aws_instance" "httpd" {
+  ami           = "${atlas_artifact.httpd.metadata_full.region-us-east-1}"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    "${aws_security_group.default_egress.id}",
+    "${aws_security_group.httpd.id}",
+  ]
+
+  tags {
+    Name = "httpd"
+  }
+}
+
+resource "aws_security_group" "default_egress" {
+  name        = "default_egress"
+  description = "Default Egress"
+}
+
+resource "aws_security_group_rule" "default_egress" {
+  security_group_id = "${aws_security_group.default_egress.id}"
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "httpd" {
+  name        = "httpd"
+  description = "HTTP"
+}
+
+resource "aws_security_group_rule" "httpd" {
+  security_group_id = "${aws_security_group.httpd.id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
